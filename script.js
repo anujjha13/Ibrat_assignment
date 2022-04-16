ee0294be74585743af64ed66625fbfe14a302d3c
Anuj <56735384+anujjha13@users.noreply.github.com>
2022-04-16 10:13:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function prev(){
+    document.getElementById('slider-container').scrollLeft -= 270;
+}
+
+function next()
+{
+    document.getElementById('slider-container').scrollLeft += 270;
+}
+
+
+
+$(".slide img").on("click" , function(){
+$(this).toggleClass('zoomed');
+$(".overlay").toggleClass('active');
+})
+
